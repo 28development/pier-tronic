319e00ab82beabfb90bb66efff5618bae3544357
Umut Tufanoglu <28development@gmail.com>
2025-10-05 23:41:32 +0200

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      'images.unsplash.com',
-      'plus.unsplash.com',
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "g3wlvawphh1gdavi.public.blob.vercel-storage.com",
     ],
   },
 };
